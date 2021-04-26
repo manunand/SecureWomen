b77b6e5b2a1d8b15ceff2a5bcb9d30cc85904fbb
shanasolutions <66355341+shanasolutions@users.noreply.github.com>
2021-04-26 17:56:13 +0530

    To Register Added

--- app/src/main/java/com/gech/securewomen/MainActivity.kt
+++ app/src/main/java/com/gech/securewomen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gech.securewomen
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -27,17 +28,10 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity","App was opened !")
             Toast.makeText(this,"Don't Be Scared You are safe now !!!".plus(name),Toast.LENGTH_LONG).show()
 
-            editTextName = findViewById(R.id.editTextName)
-            editTextNumone = findViewById(R.id.editTextNumone)
-            editTextNumtwo = findViewById(R.id.editTextNumtwo)
-            editTextNumthree = findViewById(R.id.editTextNumThree)
-            editTextNumfour = findViewById(R.id.editTextNumfour)
-            editTextNumfive = findViewById(R.id.editTextNumfive)
 
-            retrieveData()
-
-            findViewById<Button>(R.id.button).setOnClickListener {
-                saveData()
+            findViewById<Button>(R.id.buttontoregisterpage).setOnClickListener {
+                val intent = Intent(this, Toregister::class.java)
+                startActivity(intent)
             }
         }else{
             setContentView(R.layout.activity_afterregister)
@@ -48,64 +42,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun retrieveData(){
-        val mypref = getSharedPreferences("mypref", MODE_PRIVATE)
 
-        val name = mypref.getString("name","")
-        val num1 = mypref.getString("num1","")
-        val num2 = mypref.getString("num2","")
-        val num3 = mypref.getString("num3","")
-        val num4 = mypref.getString("num4","")
-        val num5 = mypref.getString("num5","")
-
-
-        editTextName.setText(name)
-        editTextNumone.setText(num1)
-        editTextNumtwo.setText(num2)
-        editTextNumthree.setText(num3)
-        editTextNumfour.setText(num4)
-        editTextNumfive.setText(num5)
-    }
-
-    private fun saveData() {
-
-        if (editTextName.text.isEmpty()){
-            editTextName.error = "Please Enter Name"
-            return
-        }
-
-        if(editTextNumone.text.isEmpty()){
-            editTextNumone.error = "Please Enter Num One"
-            return
-        }
-        if(editTextNumtwo.text.isEmpty()){
-            editTextNumtwo.error = "Please Enter Num Two"
-            return
-        }
-        if(editTextNumthree.text.isEmpty()){
-            editTextNumthree.error = "Please Enter Num Three"
-            return
-        }
-        if(editTextNumfour.text.isEmpty()){
-            editTextNumfour.error = "Please Enter Num Four"
-            return
-        }
-        if(editTextNumfive.text.isEmpty()){
-            editTextNumfive.error = "Please Enter Num Five"
-            return
-        }
-
-        val mypref = getSharedPreferences("mypref", MODE_PRIVATE)
-        val editor = mypref.edit()
-
-        editor.putString("name",editTextName.text.toString())
-        editor.putString("num1",editTextNumone.text.toString())
-        editor.putString("num2",editTextNumtwo.text.toString())
-        editor.putString("num3",editTextNumthree.text.toString())
-        editor.putString("num4",editTextNumfour.text.toString())
-        editor.putString("num5",editTextNumfive.text.toString())
-        editor.apply()
-
-        Toast.makeText(this,"Data Saved",Toast.LENGTH_LONG).show()
-    }
 }
